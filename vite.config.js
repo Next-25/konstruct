4136c3b6fb608c1f683ddaec91d27541cfc12b45
Next-25 <live.with.WT19i@gmail.com>
2025-09-17 11:31:56 +0700

    Добален визуализатор сборки и проведена сборка

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { visualizer } from 'rollup-plugin-visualizer'; // Правильный импорт
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    visualizer({ // Теперь visualizer - это функция
+      open: true, // Автоматически открыть отчет после сборки
+      filename: 'dist/stats.html', // Файл, в который сохранится отчет
+    }),
+  ],
+  build: {
+    outDir: 'dist', // Папка для сборки (по умолчанию и так 'dist')
+  }
 })
